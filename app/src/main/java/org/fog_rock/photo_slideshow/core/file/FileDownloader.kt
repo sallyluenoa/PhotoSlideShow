20ef5a269e80210db9213eed929336a2007d0f9a
SallyLueNoa <sally.lue.noa@gmail.com>
2020-03-01 23:42:10 +0900

    Implementation of Google API (#4)
    * Implement file downloader. (Use OkHttp)
    * Add junit test for file reader and downloader.

---
+++ app/src/main/java/org/fog_rock/photo_slideshow/core/file/FileDownloader.kt
@@ -0,0 +1,111 @@
+package org.fog_rock.photo_slideshow.core.file
+
+import android.util.Log
+import okhttp3.*
+import java.io.*
+import java.net.URL
+import java.util.concurrent.TimeUnit
+
+/**
+ * ファイルダウンロード専用クラス.
+ */
+class FileDownloader(
+    connectionTimeoutMilliSecs: Long,
+    readTimeoutMilliSecs: Long,
+    writeTimeoutMilliSecs: Long,
+    private val callback: Callback
+) {
+    private val TAG = FileDownloader::class.java.simpleName
+
+    interface Callback {
+        /**
+         * ダウンロード結果を返す.
+         * @param resultOutputFile ダウンロードに成功した場合は出力先ファイル、失敗した場合はNULL
+         * @see doDownload
+         */
+        fun downloadResult(resultOutputFile: File?)
+    }
+
+    private val client = OkHttpClient.Builder().apply {
+        connectTimeout(connectionTimeoutMilliSecs, TimeUnit.MILLISECONDS)
+        readTimeout(readTimeoutMilliSecs, TimeUnit.MILLISECONDS)
+        writeTimeout(writeTimeoutMilliSecs, TimeUnit.MILLISECONDS)
+    }.build()
+
+    /**
+     * ファイルダウンロードを行う.
+     * @param downloadUrl ダウンロードURL
+     * @param outputFile 出力先ファイル
+     * @see Callback.downloadResult
+     */
+    fun doDownload(downloadUrl: URL, outputFile: File) {
+        val request = Request.Builder().apply {
+            url(downloadUrl)
+        }.build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onResponse(call: Call, response: Response) {
+                Log.e(TAG, "Succeeded to get http response.")
+                val body = response.body ?: run {
+                    Log.e(TAG, "Cannot get response body.")
+                    callback.downloadResult(null)
+                    return
+                }
+                if (writeOutputFile(body, outputFile)) {
+                    Log.i(TAG, "Succeeded to write output file.")
+                    callback.downloadResult(outputFile)
+                } else {
+                    Log.e(TAG, "Failed to write output file.")
+                    callback.downloadResult(null)
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "Failed to get http response.")
+                callback.downloadResult(null)
+            }
+        })
+    }
+
+    /**
+     * レスポンスボディからファイルに書き込む.
+     * @param body レスポンスボディ
+     * @param outputFile 出力先ファイル
+     */
+    private fun writeOutputFile(body: ResponseBody, outputFile: File): Boolean {
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+
+        try {
+            inputStream = body.byteStream()
+            outputStream = FileOutputStream(outputFile)
+            inputStream.use { it.copyTo(outputStream) }
+            return true
+        } catch (e: IOException) {
+            Log.e(TAG, "Failed to write OutputStream.")
+            e.printStackTrace()
+        } catch (e: FileNotFoundException) {
+            Log.e(TAG, "Failed to open OutputStream.")
+            e.printStackTrace()
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Failed to close InputStream.")
+                    e.printStackTrace()
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.flush()
+                    outputStream.close()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Failed to close OutputStream.")
+                    e.printStackTrace()
+                }
+            }
+        }
+        return false
+    }
+}
--- app/src/main/java/org/fog_rock/photo_slideshow/core/file/FileReader.kt
+++ app/src/main/java/org/fog_rock/photo_slideshow/core/file/FileReader.kt
@@ -5,10 +5,18 @@ import android.util.Log
 import java.io.IOException
 import java.io.InputStream
 
+/**
+ * ファイル読み込み専用クラス.
+ */
 class FileReader(private val context: Context) {
 
     private val TAG = FileReader::class.java.simpleName
 
+    /**
+     * assets 以下に設置されているファイルの読み込みを行う.
+     * @param fileName ファイル名
+     * @return ファイルの読み込みに成功した場合はファイル内の文字列、失敗した場合はNULL
+     */
     fun readAssetsFile(fileName: String): String? {
         var inputStream: InputStream? = null
         val assetManager = context.resources.assets
