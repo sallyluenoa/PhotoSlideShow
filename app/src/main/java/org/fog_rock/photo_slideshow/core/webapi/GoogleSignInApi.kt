c04ffc3bc4f03865c232cbef243630fffda490ff
SallyLueNoa <sally.lue.noa@gmail.com>
2020-03-29 00:24:07 +0900

    Brush up Google APIs (#10)
    * Brash up GoogleSignInApi.

--- app/src/main/java/org/fog_rock/photo_slideshow/core/webapi/GoogleSignInApi.kt
+++ app/src/main/java/org/fog_rock/photo_slideshow/core/webapi/GoogleSignInApi.kt
@@ -1,105 +1,28 @@
 package org.fog_rock.photo_slideshow.core.webapi
 
-import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.api.ApiException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.fog_rock.photo_slideshow.core.entity.PhotoScope
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 /**
  * Googleサインインに関連するAPI.
  */
-class GoogleSignInApi(
-    private val context: Context,
-    private val clientHolder: GoogleSignInClientHolder,
-    private val callback: Callback
-) {
-
-    private val TAG = GoogleSignInApi::class.java.simpleName
-
-    interface Callback {
-
-        /**
-         * サイレントサインインを要求に成功したか.
-         * @see requestSilentSignIn
-         */
-        fun requestSilentSignInResult(isSucceeded: Boolean)
-    }
-
-    constructor(
-        context: Context, scopes: Array<PhotoScope>,
-        requestIdToken: Boolean, requestServerAuthCode: Boolean, callback: Callback
-    ): this(
-        context, GoogleSignInClientHolder(context, scopes, requestIdToken, requestServerAuthCode), callback
-    )
+interface GoogleSignInApi {
 
     /**
      * Googleアカウントでのユーザーサインインに成功したか.
      */
-    fun isSucceededUserSignIn(data: Intent?): Boolean =
-        GoogleSignIn.getSignedInAccountFromIntent(data).isSuccessful
+    fun isSucceededUserSignIn(data: Intent?): Boolean
 
     /**
      * サイレントサインイン要求.
+     * コルーチン内で呼び出すこと.
      * https://developers.google.com/identity/sign-in/android/backend-auth
-     * @see Callback.requestSilentSignInResult
      */
-    fun requestSilentSignIn() {
-        GlobalScope.launch(Dispatchers.Main) {
-            if (GoogleSignIn.getLastSignedInAccount(context) == null) {
-                Log.i(TAG, "Sign out now.")
-                callback.requestSilentSignInResult(false)
-                return@launch
-            }
-            if (!withContext(Dispatchers.Default) { silentSignIn() }) {
-                Log.e(TAG, "Failed to silent sign in.")
-                callback.requestSilentSignInResult(false)
-                return@launch
-            }
-            Log.i(TAG, "Succeeded to silent sign in.")
-            callback.requestSilentSignInResult(true)
-        }
-    }
+    suspend fun requestSilentSignIn(): Boolean
 
     /**
-     * サイレントサインイン.
-     * Suspendメソッドなので、Coroutine内で呼び出すこと.
+     * サインアウト要求.
+     * コルーチン内で呼び出すこと.
+     * https://developers.google.com/identity/sign-in/android/disconnect
      */
-    private suspend fun silentSignIn(): Boolean =
-        suspendCoroutine { continuation ->
-            clientHolder.client.silentSignIn().apply {
-                addOnCompleteListener {
-                    Log.i(TAG, "Completed to silent sign in.")
-                    try {
-                        val account = it.getResult(ApiException::class.java) ?: run {
-                            Log.e(TAG, "Failed to get account from result task.")
-                            continuation.resume(false)
-                            return@addOnCompleteListener
-                        }
-                        Log.i(TAG, "Succeeded to get account from result task.")
-                        continuation.resume(true)
-                    } catch (e: ApiException) {
-                        e.printStackTrace()
-                        continuation.resume(false)
-                    }
-                }
-                addOnFailureListener {
-                    Log.e(TAG, "Failed to silent sign in.")
-                    it.printStackTrace()
-                    continuation.resume(false)
-                }
-                addOnCanceledListener {
-                    Log.e(TAG, "Canceled to silent sign in.")
-                    continuation.resume(false)
-                }
-            }
-            return@suspendCoroutine
-        }
+    suspend fun requestSignOut(): Boolean
 }
---
+++ app/src/main/java/org/fog_rock/photo_slideshow/core/webapi/impl/GoogleSignInApiImpl.kt
@@ -0,0 +1,109 @@
+package org.fog_rock.photo_slideshow.core.webapi.impl
+
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import org.fog_rock.photo_slideshow.core.webapi.GoogleSignInApi
+import org.fog_rock.photo_slideshow.core.webapi.GoogleSignInClientHolder
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class GoogleSignInApiImpl(
+    private val clientHolder: GoogleSignInClientHolder
+): GoogleSignInApi {
+
+    private val TAG = GoogleSignInApiImpl::class.java.simpleName
+
+    override fun isSucceededUserSignIn(data: Intent?): Boolean =
+        GoogleSignIn.getSignedInAccountFromIntent(data).isSuccessful
+
+    override suspend fun requestSilentSignIn(): Boolean {
+        val account = silentSignIn()
+        return account != null
+    }
+
+    override suspend fun requestSignOut(): Boolean {
+        val isSignedOut = signOut()
+        val isRevokedAccess = revokeAccess()
+        return isSignedOut
+    }
+
+    /**
+     * サイレントサインイン.
+     */
+    private suspend fun silentSignIn(): GoogleSignInAccount? =
+        suspendCoroutine { continuation ->
+            var account: GoogleSignInAccount? = null
+            clientHolder.client.silentSignIn().apply {
+                addOnSuccessListener {
+                    Log.i(TAG, "Succeeded to silent sign in.")
+                    account = it
+                }
+                addOnFailureListener {
+                    Log.e(TAG, "Failed to silent sign in.")
+                    it.printStackTrace()
+                }
+                addOnCanceledListener {
+                    Log.e(TAG, "Canceled to silent sign in.")
+                }
+                addOnCompleteListener {
+                    Log.i(TAG, "Completed to silent sign in.")
+                    continuation.resume(account)
+                }
+            }
+            return@suspendCoroutine
+        }
+
+    /**
+     * サインアウト.
+     */
+    private suspend fun signOut(): Boolean =
+        suspendCoroutine { continuation ->
+            var isSignedOut = false
+            clientHolder.client.signOut().apply {
+                addOnSuccessListener {
+                    Log.i(TAG, "Succeeded to sign out.")
+                    isSignedOut = true
+                }
+                addOnFailureListener {
+                    Log.e(TAG, "Failed to sign out.")
+                    it.printStackTrace()
+                }
+                addOnCanceledListener {
+                    Log.e(TAG, "Canceled to sign out.")
+                }
+                addOnCompleteListener {
+                    Log.i(TAG, "Completed to sign out.")
+                    continuation.resume(isSignedOut)
+                }
+            }
+            return@suspendCoroutine
+        }
+
+    /**
+     * アカウントのアクセス破棄.
+     */
+    private suspend fun revokeAccess(): Boolean =
+        suspendCoroutine { continuation ->
+            var isRevokedAccess = false
+            clientHolder.client.revokeAccess().apply {
+                addOnSuccessListener {
+                    Log.i(TAG, "Succeeded to revoke access.")
+                    isRevokedAccess = true
+                }
+                addOnFailureListener {
+                    Log.e(TAG, "Failed to revoke access.")
+                    it.printStackTrace()
+                }
+                addOnCanceledListener {
+                    Log.e(TAG, "Canceled to revoke access.")
+                }
+                addOnCompleteListener {
+                    Log.i(TAG, "Completed to revoke access.")
+                    continuation.resume(isRevokedAccess)
+                }
+            }
+            return@suspendCoroutine
+        }
+}
