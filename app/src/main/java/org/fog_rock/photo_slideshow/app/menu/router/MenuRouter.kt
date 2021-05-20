e71dce7a800b6e08a35ce869ef91af16893c9858
SallyLueNoa <sally.lue.noa@gmail.com>
2021-05-21 00:23:54 +0900

    Refactoring: Add ActivityExtension for flowing screen.

--- app/src/main/java/org/fog_rock/photo_slideshow/app/menu/router/MenuRouter.kt
+++ app/src/main/java/org/fog_rock/photo_slideshow/app/menu/router/MenuRouter.kt
@@ -1,21 +1,20 @@
 package org.fog_rock.photo_slideshow.app.menu.router
 
 import android.app.Activity
-import android.content.Intent
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import org.fog_rock.photo_slideshow.app.menu.contract.MenuContract
+import org.fog_rock.photo_slideshow.app.module.ui.extension.newIntent
+import org.fog_rock.photo_slideshow.app.module.ui.extension.startActivityAndFinishAll
 import org.fog_rock.photo_slideshow.app.splash.view.SplashActivity
 
 class MenuRouter : MenuContract.Router {
 
     override fun startSplashActivityAndFinishAll(activity: Activity) {
-        activity.startActivity(Intent(activity, SplashActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        })
+        activity.startActivityAndFinishAll<SplashActivity>()
     }
 
     override fun startOssLicensesMenuActivity(activity: Activity, titleResId: Int) {
-        activity.startActivity(Intent(activity, OssLicensesMenuActivity::class.java).apply {
+        activity.startActivity(activity.newIntent<OssLicensesMenuActivity>().apply {
             putExtra("title", activity.getString(titleResId))
         })
     }
