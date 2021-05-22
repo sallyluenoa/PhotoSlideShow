21bf020c5a18e43ec3d600ee159aabf61b5624b4
SallyLueNoa <sally.lue.noa@gmail.com>
2021-05-22 15:16:45 +0900

    Refactoring

--- app/src/main/java/org/fog_rock/photo_slideshow/app/module/ui/extension/FragmentActivityExtension.kt
+++ app/src/main/java/org/fog_rock/photo_slideshow/app/module/ui/extension/FragmentActivityExtension.kt
@@ -1,20 +1,21 @@
 package org.fog_rock.photo_slideshow.app.module.ui.extension
 
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import org.fog_rock.photo_slideshow.R
 
-fun FragmentActivity.addFragment(fragment: Fragment) {
-    replaceFragment(fragment, true)
+fun FragmentActivity.addFragment(fragment: Fragment, @IdRes resId: Int = R.id.fragment_container) {
+    replaceFragment(fragment, resId, true)
 }
 
-fun FragmentActivity.replaceFragment(fragment: Fragment) {
-    replaceFragment(fragment, false)
+fun FragmentActivity.replaceFragment(fragment: Fragment, @IdRes resId: Int = R.id.fragment_container) {
+    replaceFragment(fragment, resId, false)
 }
 
-private fun FragmentActivity.replaceFragment(fragment: Fragment, addStack: Boolean) {
+private fun FragmentActivity.replaceFragment(fragment: Fragment, @IdRes resId: Int, addStack: Boolean) {
     supportFragmentManager.beginTransaction().apply {
-        replace(R.id.fragment_container, fragment)
+        replace(resId, fragment)
         if (addStack) addToBackStack(null)
     }.commit()
 }
